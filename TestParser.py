5682cb8cc38e886ad1315e331b8bb8f1efb8391d
Jing Wen <jwchong93@gmail.com>
2014-10-24 17:13:17 +0800

    Provide a test for parser, to verify the usablility of the codes.

--- TestParser.py
+++ TestParser.py
@@ -160,5 +160,120 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(4, token.data[0].data[1].data[1].data[0])
         self.assertEqual(5, token.data[1].data[0])
 
+
+    def test_parse_negation_2_plus_3(self):
+        """
+                +
+              /   \
+             !     3
+           /
+          2
+        :return:
+        """
+        context = Context()
+        context.addPrefixOperator('!',120)
+        context.addInfixOperator('+', 70)
+        context.addInfixOperator('(end)', 0)
+
+        parser = Parser('! 2 + 3 ', [context])
+        context.setParser(parser)
+        token = parser.parse(0)
+        self.assertEqual('+', token.id)
+        self.assertEqual('!', token.data[0].id)
+        self.assertEqual( 2 , token.data[0].data[0].data[0])
+        self.assertEqual( 3 , token.data[1].data[0])
+
+
+    def test_parse_negative_2_plus_3(self):
+        """
+                +
+              /   \
+             -     3
+           /
+          2
+        :return:
+        """
+        context = Context()
+        context.addPrefixOperator('-',120)
+        context.addInfixOperator('+', 70)
+        context.addInfixOperator('(end)', 0)
+
+        parser = Parser('- 2 + 3 ', [context])
+        context.setParser(parser)
+        token = parser.parse(0)
+        self.assertEqual('+', token.id)
+        self.assertEqual('-', token.data[0].id)
+        self.assertEqual( 2 , token.data[0].data[0].data[0])
+        self.assertEqual( 3 , token.data[1].data[0])
+
+
+    def test_parse_2_plus_negative_3(self):
+        """
+                +
+              /   \
+             2     -
+                    \
+                     3
+        :return:
+        """
+        context = Context()
+        context.addPrefixOperator('-',120)
+        context.addInfixOperator('+', 70)
+        context.addInfixOperator('(end)', 0)
+
+        parser = Parser(' 2 + - 3 ', [context])
+        context.setParser(parser)
+        token = parser.parse(0)
+        self.assertEqual('+', token.id)
+        self.assertEqual(2, token.data[0].data[0])
+        self.assertEqual( 3 , token.data[1].data[0].data[0])
+        self.assertEqual( '-' , token.data[1].id)
+
+
+    def test_parse_2_minus_negative_3(self):
+        """
+                -
+              /   \
+             2     -
+                    \
+                     3
+        :return:
+        """
+        context = Context()
+        context.addPrefixOperator('-',120)
+        context.addInfixOperator('-', 70)
+        context.addInfixOperator('(end)', 0)
+
+        parser = Parser(' 2 - - 3 ', [context])
+        context.setParser(parser)
+        token = parser.parse(0)
+        self.assertEqual('-', token.id)
+        self.assertEqual(2, token.data[0].data[0])
+        self.assertEqual( '-' , token.data[1].id)
+        self.assertEqual( 3 , token.data[1].data[0].id)
+
+    def test_parse_negative_2_minus_3(self):
+        """
+                -
+              /   \
+             -     3
+           /
+          2
+        :return:
+        """
+        context = Context()
+        classMinus = context.addInfixOperator('-', 70)
+        classMinus.nud = nud
+        context.addInfixOperator('(end)', 0)
+
+        parser = Parser(' - 2 - 3 ', [context])
+        context.setParser(parser)
+        token = parser.parse(0)
+        self.assertEqual('-', token.id)
+        self.assertEqual('-', token.data[0].id)
+        self.assertEqual( 3 , token.data[1].data[0])
+        self.assertEqual( 2 , token.data[0].data[0].data[0])
+
+
 if __name__ == '__main__':
     unittest.main()
